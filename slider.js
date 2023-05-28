c2d70f4be5d4f1ff8d7268bb9e735d14b7d99c40
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2023-05-28 15:03:41 +0300

    update style files

--- slider.js
+++ slider.js
@@ -28,6 +28,50 @@ const slides=[
 
 ];
 
+const certifs=[
+    {
+        "img":"./images/certifics/cisco.jpeg",
+        "title": "Cisco Cyber Security Intro",
+        "description": ""
+      },
+    {
+        "img":"./images/certifics/solopy1.jpeg",
+        "title": "Sololearn Python Intro",
+        "description": "This is my Python certf."
+      },
+   
+      
+    {
+        "img":"./images/certifics/globpy1.jpeg",
+        "title": "Global AI HUB Python Bootcamp",
+        "description": "This is my Python Bootcamp certf."
+      },
+
+    {
+        "img":"./images/certifics/gitgithub.jpeg",
+        "title": "BTK Academy Git and Github",
+        "description": "This is my Git and Github certf."
+      },
+    {
+        "img":"./images/certifics/solopy2.jpeg",
+        "title": "Python Data Structures",
+        "description": "This is my Python Data Structures certf."
+      }
+
+];
+
+let count=0;
+function changeCertifices(){
+  const slide=certifs[count];
+  const img=document.querySelector(".card2 img");
+  const title = document.querySelector('.content2 h2');
+  const content = document.querySelector('.content2 p');
+  img.src=slide.img;
+  title.textContent=slide.title;
+  content.textContent=slide.description;
+  count=(count+1)%certifs.length;
+}
+
 let index=0;
 function changeSlide(){
     const slide=slides[index];
@@ -41,7 +85,7 @@ function changeSlide(){
     index=(index+1)%slides.length;
 
 }
-
+    changeCertifices();
     changeSlide();
-
+    setInterval(changeCertifices,5000);
     setInterval(changeSlide, 5000);
